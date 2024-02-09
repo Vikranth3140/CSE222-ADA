4e9d73e0a489fe1710905e182440b0bc9608998e
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-10 00:45:24 +0530

    added new algo O(n)

--- Assignments/A2/a2.cpp
+++ Assignments/A2/a2.cpp
@@ -1,40 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-int maxChickens(vector<int>& A) {
-    int n = A.size();
+int max_chickens_earned(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 0) return 0;
     
-    vector<vector<vector<int>>> DP(n + 1, vector<vector<int>>(2, vector<int>(4, 0)));
-    
-    DP[1][0][1] = max(A[0], 0);
-    DP[1][1][1] = max(-A[0], 0);
-
-    for (int i = 2; i <= n; ++i) {
-        for (int j = 0; j <= 1; ++j) {
-            for (int k = 1; k <= 3; ++k) {
-                int reward_penalty = (j == 0) ? (A[i - 1] > 0 ? A[i - 1] : -A[i - 1]) : (A[i - 1] < 0 ? -A[i - 1] : A[i - 1]);
-                
-                if (k < 3) {
-                    DP[i][j][k] = max(DP[i - 1][j][k + 1] + reward_penalty, DP[i - 1][1 - j][1] + reward_penalty);
-                } else {
-                    DP[i][j][k] = DP[i - 1][1 - j][1] + reward_penalty;
-                }
-            }
+    vector<int> dp(n, 0);
+    int count = 0;
+    for (int ind = 1; ind < n; ind++) {
+        if (count == 2) {
+            dp[ind] = -dp[ind-1];
+            count = 0;
+            continue;
         }
+        int ring = dp[ind-1] + (nums[ind] > 0 ? nums[ind] : -nums[ind]);
+        int ding = dp[ind-1] + (nums[ind] < 0 ? nums[ind] : -nums[ind]);
+        dp[ind] = max(ring, ding);
+        count++;
     }
-    
-    return max(DP[n][0][1], DP[n][1][1]);
+    return dp[n-1];
 }
 
 int main() {
-    vector<int> A = {1, -2, 3, -4, 5};
-    
-    int maxChickensEarned = maxChickens(A);
-    
-    cout << "Maximum number of chickens earned by Mr. Fox: " << maxChickensEarned << endl;
-    
+    vector<int> nums = {3, -1, 4, -5, 6, -7, 8};
+    cout << max_chickens_earned(nums) << endl;
     return 0;
 }
