8aacb810730288ddb771b15c5a7da68fd8087f5d
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-10 13:40:23 +0530

    added O(n) cpp code

--- Assignments/A2/a2.cpp
+++ Assignments/A2/a2.cpp
@@ -1,30 +1,47 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include<vector>
+#include<climits>
+
 using namespace std;
 
-int max_chickens_earned(vector<int>& nums) {
-    int n = nums.size();
-    if (n == 0) return 0;
-    
-    vector<int> dp(n, 0);
-    int count = 0;
-    for (int ind = 1; ind < n; ind++) {
-        if (count == 2) {
-            dp[ind] = -dp[ind-1];
-            count = 0;
-            continue;
+int check(vector<int> & nums,int ind,int rc,int dc,vector<vector<vector<int>>> dp){
+    if(ind==0){
+        if(rc==3){
+            return -nums[ind];
         }
-        int ring = dp[ind-1] + (nums[ind] > 0 ? nums[ind] : -nums[ind]);
-        int ding = dp[ind-1] + (nums[ind] < 0 ? nums[ind] : -nums[ind]);
-        dp[ind] = max(ring, ding);
-        count++;
+        if(dc==3){
+            return nums[ind];
+        }
+        if(nums[ind]>0){
+            return nums[ind];
+        }
+        else if(nums[ind]<0){
+            return -nums[ind];
+        }
     }
-    return dp[n-1];
+    if(dp[ind][rc][dc]!=INT_MIN){
+        return dp[ind][rc][dc];
+    }
+    int ring=INT_MIN;
+    int ding=INT_MIN;
+    if(rc==3){
+        ding=check(nums,ind-1,0,1,dp)-nums[ind];
+    }
+    else if(dc==3){
+        ring=check(nums,ind-1,1,0,dp)+nums[ind];
+    }
+    else{
+        ring=check(nums,ind-1,rc+1,0,dp)+nums[ind];
+        ding=check(nums,ind-1,0,dc+1,dp)-nums[ind];
+    }
+    dp[ind][rc][dc]=max(ring,ding);
+    return max(ring,ding);
 }
-
 int main() {
-    vector<int> nums = {3, -1, 4, -5, 6, -7, 8};
-    cout << max_chickens_earned(nums) << endl;
+    vector<int> nums{2, 4, 5, 6, -3, -7, 1, -4, 5, -7};
+    int n=nums.size();
+    vector<vector<int>> a(4,vector<int> (4,INT_MIN));
+    vector<vector<vector<int>>> dp (n,a);
+    cout<<check(nums,n-1,0,0,dp);
     return 0;
 }
