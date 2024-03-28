9ac2feeb0447b3730eb6f7664096d385d0575aa3
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-03-29 01:38:23 +0530

    Added Assignment 4 code

---
+++ Assignments/A4/a4.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <stack>
+
+void topological_sort(int node,stack<int> &st, vector<int> &visited,vector<int> adj[]){
+    visited[node]=1;
+    for(auto i: adj[node]){
+        if(visited[i]==0){
+            topological_sort(i,st,visited,adj);
+        }
+    }
+    st.push(node);
+    return
+}
+
+void cutNodeFinder(int node,vector<int> &start,vector<int> &end,vector<int> &cut_nodes,vector<int> adj[]){
+    start[node]=1;
+    for(auto i: adj[node]){
+        if(start[i]==0){
+            cutNodeFinder(i,start,end,cut_nodes,adj);
+        }
+        else if(start[i]==1 && end[i]==0){
+            cut_nodes.push_back(i);
+        }
+    }
+    end[node]=1;
+
+    return;
+}
+
+int main(){
+    vector<int> visited_topo(n,0);
+    vector<int> adj[n];
+    stack<int> st;
+
+    // Assume nodes from 0 to n-1
+    for(int i=0;i<=n-1;i++){
+        if(visited_topo[i]==0){
+            topological_sort(i,st,visited_topo,adj);
+        }
+    }
+
+    // Stack now has topologically sorted vertices.
+    vector<int> start(n,0);
+    vector<int> end(n,0);
+    vector<int> cut_nodes;
+
+    while(!st.empty()){
+        int node=st.top();
+        st.pop();
+        if (start[node]==0){
+            cutNodeFinder(node,start,end,cut,adj);
+        }
+    }
+
+    // Cut_nodes stroes answer.
+    return 0
+}
