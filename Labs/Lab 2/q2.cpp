9e2ec17fe94b90c28a2ff0019b6ba1f8d7171c88
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-04-07 01:19:33 +0530

    Lab 2 B - Memoisation

--- Labs/Lab 2/q2.cpp
+++ Labs/Lab 2/q2.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
-const int MOD = 1e9+7;
+const int MOD = 1e9 + 7;
+// Here, we have to use std::unordered_map because without using it I had got compilation error
+std::unordered_map<long long, int> memo;
+
+long long make_key(int i, int current_OR) {
+    return ((long long)i << 32) | current_OR;
+}
 
 int countSubsequences(int i, int current_OR, int x, const std::vector<int>& numbers) {
     if (i == numbers.size()) {
         return current_OR == x;
     }
     
-    int include = countSubsequences(i + 1, current_OR | numbers[i], x, numbers);
+    long long key = make_key(i, current_OR);
+    if (memo.find(key) != memo.end()) {
+        return memo[key];
+    }
     
+    int include = countSubsequences(i + 1, current_OR | numbers[i], x, numbers);
+
     int exclude = countSubsequences(i + 1, current_OR, x, numbers);
     
-    return (include + exclude) % MOD;
+    memo[key] = (include + exclude) % MOD;
+    return memo[key];
 }
 
 int main() {
@@ -30,6 +43,7 @@ int main() {
         if (M == 1) {
             numbers.push_back(x);
         } else {
+            memo.clear();
             // Here, we have to use std::cout because without using it I had got compilation error
             std::cout << countSubsequences(0, 0, x, numbers) << std::endl;
         }
