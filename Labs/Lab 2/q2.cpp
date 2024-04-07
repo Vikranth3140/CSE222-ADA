391d467ef542d6ce067b0e060ce9d46a19d5f8d8
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-04-07 14:44:33 +0530

    Lab 2 B Accepted

--- Labs/Lab 2/q2.cpp
+++ Labs/Lab 2/q2.cpp
@@ -2,27 +2,21 @@
 #include <vector>
 
 const int MOD = 1e9 + 7;
+const int MAX_X = 1024;
 
-// Dint use Memoization DP
-// Tried to use bitmasking as dynamic approach in previous submission gave time limit exceeded at test case 3
-int countValidSubsequences(int x, const std::vector<int>& numbers) {
-    int count = 0;
-    
-    for (int mask = 0; mask < (1 << numbers.size()); ++mask) {
-        int current_OR = 0;
-        
-        for (int i = 0; i < numbers.size(); ++i) {
-            if (mask & (1 << i)) {
-                current_OR |= numbers[i];
-            }
-        }
-        
-        if (current_OR == x) {
-            ++count;
-            count %= MOD;
+// Went back to DP as Time Complexity of Bitmasking is O(2^n)
+void updateDP(std::vector<std::vector<int>>& dp, const std::vector<int>& numbers, int start_i) {
+    int n = numbers.size();
+    for (int i = start_i; i < n; ++i) {
+        for (int j = 0; j < MAX_X; ++j) {
+            int new_OR = j | numbers[i];
+            dp[i + 1][new_OR] += dp[i][j];
+            dp[i + 1][new_OR] %= MOD;
+
+            dp[i + 1][j] += dp[i][j];
+            dp[i + 1][j] %= MOD;
         }
     }
-    return count;
 }
 
 int main() {
@@ -31,17 +25,28 @@ int main() {
     std::cin >> Q;
     // Here, we have to use std::vector because without using it I had got compilation error
     std::vector<int> numbers;
-    
+    // Here, we have to use std::vector because without using it I had got compilation error
+    std::vector<std::vector<int>> dp(1, std::vector<int>(MAX_X, 0));
+
+    dp[0][0] = 1;
+    int updated_i = 0;
+
     while (Q--) {
         int M, x;
         // Here, we have to use std::cin because without using it I had got compilation error
         std::cin >> M >> x;
-        
+
         if (M == 1) {
             numbers.push_back(x);
         } else {
-            // Here, we have to use std::cout because without using it I had got compilation error
-            std::cout << countValidSubsequences(x, numbers) << std::endl;
+            if (numbers.size() > dp.size() - 1) {
+                // Here, we have to use std::vector because without using it I had got compilation error
+                dp.resize(numbers.size() + 1, std::vector<int>(MAX_X, 0));
+                updateDP(dp, numbers, updated_i);
+                updated_i = numbers.size();
+            }
+            // Here, we have to use std::cout and std::endl because without using it I had got compilation error
+            std::cout << dp[numbers.size()][x] << std::endl;
         }
     }
     return 0;
